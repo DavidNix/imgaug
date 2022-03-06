ea39e11c2414af42183fbe61fc62f5cfdfe80fd9
David Nix <hello@davidnix.io>
2022-03-06 12:00:52 -0700

    first mvp

--- aug/transform.go
+++ aug/transform.go
@@ -1,6 +1,8 @@
 package aug
 
 import (
+	"github.com/anthonynsimon/bild/adjust"
+	"github.com/anthonynsimon/bild/effect"
 	"github.com/anthonynsimon/bild/imgio"
 	"github.com/anthonynsimon/bild/transform"
 	"github.com/google/uuid"
@@ -18,22 +20,30 @@ func (tf *Transformer) Augment(in <-chan image.Image) int64 {
 	for orig := range in {
 		for _, img := range tf.baseImages(orig) {
 			tf.savePNG(img)
+			for _, v := range []float64{-0.3, -0.15, 0.15, 0.3} {
+				var (
+					bright   = adjust.Brightness(img, v)
+					contrast = adjust.Contrast(img, v)
+				)
+				tf.savePNG(bright)
+				tf.savePNG(contrast)
+				tf.savePNG(adjust.Brightness(contrast, v))
+				tf.savePNG(adjust.Contrast(bright, v))
+			}
 		}
 	}
 	return tf.total
 }
 
 func (tf *Transformer) baseImages(orig image.Image) (images []image.Image) {
-	for _, img := range []image.Image{orig, transform.FlipH(orig)} {
+	for _, img := range []image.Image{effect.Sharpen(orig), effect.Sharpen(transform.FlipH(orig))} {
 		images = append(
 			images,
 			img,
 			transform.Rotate(img, -10, nil),
 			transform.Rotate(img, -20, nil),
-			transform.Rotate(img, -30, nil),
 			transform.Rotate(img, 10, nil),
 			transform.Rotate(img, 20, nil),
-			transform.Rotate(img, 30, nil),
 		)
 	}
 	return images
@@ -46,4 +56,16 @@ func (tf *Transformer) savePNG(img image.Image) {
 		return
 	}
 	tf.total++
+}
+
+func (tf *Transformer) adjustBrightness(img image.Image) {
+	for _, v := range []float64{-0.3, -0.15, 0.15, 0.3} {
+		tf.savePNG(adjust.Brightness(img, v))
+	}
+}
+
+func (tf *Transformer) adjustContrast(img image.Image) {
+	for _, v := range []float64{-0.3, -0.15, 0.15, 0.3} {
+		tf.savePNG(adjust.Contrast(img, v))
+	}
 }
